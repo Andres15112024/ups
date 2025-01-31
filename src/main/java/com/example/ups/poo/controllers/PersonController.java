de1628a9bee2fa90f12352c4c8f8dca88bd1abf2
Andres15112004 <aperaltac9@est.ups.edu.ec>
2025-01-30 23:34:53 -0500

    Add(I): Edit Lombok Dependency

--- src/main/java/com/example/ups/poo/controllers/PersonController.java
+++ src/main/java/com/example/ups/poo/controllers/PersonController.java
@@ -2,8 +2,9 @@ package com.example.ups.poo.controllers;
 
 import com.example.ups.poo.dto.Person;
 import com.example.ups.poo.service.PersonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +17,18 @@ public class PersonController {
     }
 
     @GetMapping("/get-all-people")
-    public List<Person> getAllPeople(){
+    public ResponseEntity getAllPeople() {
         return personService.getAllPeople();
+    }
+
+    @GetMapping("/get-person")
+    public ResponseEntity getPersonById(@RequestParam String id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping("/person")
+    public ResponseEntity createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
 
--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -1,6 +1,8 @@
 package com.example.ups.poo.service;
 
 import com.example.ups.poo.dto.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,15 +13,42 @@ public class PersonService {
 
     List<Person> personList = new ArrayList<>();
 
-    public List<Person> getAllPeople() {
-        Person p1 = new Person();
-        p1.setName("Andres");
-        p1.setLastname("Peralta");
-        p1.setAge(20);
-        p1.setId("0931260905");
-        Person p2 = new Person("Wilson", "Peralta", 65, "1801541937");
-        personList.add(p1);
-        personList.add(p2);
-        return personList;
+    public ResponseEntity getAllPeople() {
+        if (personList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person List is empty");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(personList);
+    }
+
+    public ResponseEntity getPersonById(String id) {
+        for (Person person : personList) {
+            if (id.equalsIgnoreCase(person.getId())) {
+                return ResponseEntity.status(HttpStatus.OK).body(person);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id; " + id + " not found");
+    }
+
+    public ResponseEntity createPerson(Person person) {
+        if (person.getId() == null || person.getId().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: ID is required.");
+        }
+        if (person.getName() == null || person.getName().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Name is required.");
+        }
+        if (person.getLastname() == null || person.getLastname().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Last Name is required.");
+        }
+        if (person.getAge() <= 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Age is required.");
+        }
+
+        for (Person existingPerson : personList) {
+            if (existingPerson.getId().equalsIgnoreCase(person.getId())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Person with ID " + person.getId() + " already exists.");
+            }
+        }
+        personList.add(person);
+        return ResponseEntity.status(HttpStatus.OK).body("Person successfully registered.");
     }
 }
